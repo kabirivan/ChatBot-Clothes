7ecd729d7a0320162584e2947eeddf1e7a7526b8
IVAN <aguas.xavier2013a@gmail.com>
2021-08-16 21:51:49 -0500

    add account model

--- backend/category/models.py
+++ backend/category/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
     cat_image = models.ImageField(upload_to='photos/categories', blank=True)
 
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.category_name
--- backend/chatbot/settings.py
+++ backend/chatbot/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chatbot.wsgi.application'
-
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
